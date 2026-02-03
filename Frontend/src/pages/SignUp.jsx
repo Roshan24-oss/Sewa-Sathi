f2815bbc90cfe92e620f001a144cf726d45b20f6
Roshan Khadayat <roshankhadayat72052@gmail.com>
2026-02-03 21:39:25 +0545

    signup page updated

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>
-      This is Sign Up page
-      
+    <div className='bg-amber-200 h-screen flex items-center justify-center' >
+      <div className='border-4 h-90 w-90 '>
+
+<form>
+  <input type="text"  placeholder='Roshan..' className='border-4 m-4' />
+</form>
+
+      </div>
+
     </div>
   )
 }
