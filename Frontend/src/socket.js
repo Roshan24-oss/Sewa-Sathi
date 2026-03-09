347e48241cd22deed9630bf565df922f781ccf01
Roshan Khadayat <roshankhadayat72052@gmail.com>
2026-03-09 21:19:42 +0545

    Add cors for local and production url

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,7 +1,13 @@
-import {io} from 'socket.io-client';
-const socket=io("https://sewa-sathi-backend-jxrg.onrender.com",{
-    withCredentials:true,
+import { io } from "socket.io-client";
 
+
+const SOCKET_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8000" 
+    : "https://sewa-sathi-backend-jxrg.onrender.com"; 
+
+const socket = io(SOCKET_URL, {
+  withCredentials: true,
 });
 
 export default socket;
